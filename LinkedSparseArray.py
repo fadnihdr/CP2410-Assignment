41d3cd45592ce3999f103c5489dc1d5f4f06577e
Fadni <fadnihdr@gmail.com>
2017-05-04 14:15:44 +0800

    added time get_usage fix

--- LinkedSparseArray.py
+++ LinkedSparseArray.py
@@ -1,35 +1,50 @@
+import time
+
 class LinkedSparseArray:
-    def __init__(self):
-        self._n = 10
+    def __init__(self,n):
+        self._n = n
         self._array = [None] * self._n
 
     def __len__(self):
         return self._n
 
     def __getitem__(self, j):
-        if not 0 <= j < self._n:
+        if not -1 <= j < self._n:
             raise IndexError('Invalid index')
-        else:
-            if j == -1:
-                return self._array[self._n - 1]
-            return self._array[j]
+        elif j == -1:
+            return self._array[self._n - 1]
+        return self._array[j]
 
     def get_usage(self):
         counter = 0
         for i in self._array:
-            if i is None:
+            if i is not None:
                 counter += 1
         return counter
 
     def __setitem__(self, j, e):
-        if not 0 <= j < self._n:
+        if not -1 <= j < self._n:
             raise IndexError('Invalid Index')
+        elif j == -1:
+            self._array[self._n - 1] = e
         self._array[j] = e
 
 
 if __name__ == "__main__":
-    l = LinkedSparseArray()
+    start = time.time()
+    l = LinkedSparseArray(10 ** 9)
+    print("Get usage")
     print(l.get_usage())
+    print("Setting Item....")
     l.__setitem__(0, 1)
+    print("Done!")
+    print("Getting item")
     print(l.__getitem__(0))
+    print("Updated get_usage is")
+    print(l.get_usage())
+    print("Array Length is")
     print(l.__len__())
+    print("Last item is")
+    print(l.__getitem__(-1))
+    end = time.time()
+    print (end - start)
